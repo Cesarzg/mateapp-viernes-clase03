c4d39d2524aad5dacfe044d78b3564c434807aed
Cesarzg <76677457@certus.edu.pe>
2021-04-23 20:38:21 -0500

    nuevo repositorio

---
+++ Actividad2.py
@@ -0,0 +1,10 @@
+#concatenacion
+a=14
+b=12
+c=a+b
+print(a+b)
+print(a,"hola", b, '=', c)
+#print(a+"hola"+ b+'='+ c)
+igual= '='
+#A la hora de concatenar numero y cadenas de texto, es necesario convertir numeros a variables de texto:
+print(str(a)+ "hola"+str(b)+'='+str(c))
