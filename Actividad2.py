1f623652eb2711cceb2f3125e6ba3df0b1debc64
Cesarzg <76677457@certus.edu.pe>
2021-04-24 21:49:37 -0500

    repositorio

--- Actividad2.py
+++ Actividad2.py
@@ -8,3 +8,4 @@ print(a,"hola", b, '=', c)
 igual= '='
 #A la hora de concatenar numero y cadenas de texto, es necesario convertir numeros a variables de texto:
 print(str(a)+ "hola"+str(b)+'='+str(c))
+
